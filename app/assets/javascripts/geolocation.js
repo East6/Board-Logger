56f5f991714fb30d96ec2672cada19d90a84f675
Higashi Yonamine <e165714@ie.u-ryukyu.ac.jp>
2018-12-19 16:41:24 +0900

    エラーメッセージ途中(マージしたい)

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,4 +1,4 @@
-window.onpageshow = function geoFindMe() {
+window.onload = function geoFindMe() {
   var output = document.getElementById("out");
   if (!navigator.geolocation){
     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
@@ -10,38 +10,39 @@ window.onpageshow = function geoFindMe() {
     var longitude = position.coords.longitude;
     document.getElementById("latitude").value = latitude;
     document.getElementById("longitude").value = longitude;
-
+    
     //ここで呼び出したい。
-
     //$.ajax(url: 'positions/create', type: "POST", data:{latitude,longitude});
     //$.post("/positions/create", {data:1.6,authenticity_token: getCSRFtoken()})
     //$.post("/boards/create",  {data:latitude,longitude,authenticity_token: getCSRFtoken()})
-
     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-    window.open("https://maps.google.com/maps?q=" + latitude +"," + longitude ,'_blank')
   }
-
   function error() {
+    alert("エラー表示");
     output.innerHTML = "Unable to retrieve your location";
+    errorCallback(error);
+  }
+  output.innerHTML = "<p>Locating…</p>";
+  navigator.geolocation.getCurrentPosition(success, error);
+  function errorCallback(error){
+    alert("エラー表示(エラーメッセージの中)");
     var err_msg = "";
+    alert(error.code)
     switch(error.code)
     {
       case 1:
-        err_msg = "位置情報の利用が許可されていません";
+        alert("位置情報の利用が許可されていません");
+        //err_msg = "位置情報の利用が許可されていません";
         break;
       case 2:
+        alert("位置情報の利用が許可されていません");
         err_msg = "デバイスの位置が判定できません";
         break;
       case 3:
+        alert("位置情報の利用が許可されていません");
         err_msg = "タイムアウトしました";
         break;
-    }
+      }
     document.getElementById("out").innerHTML = err_msg;
-    //デバック用 → document.getElementById("out").innerHTML = error.message;
   }
-debugger;
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
 }
